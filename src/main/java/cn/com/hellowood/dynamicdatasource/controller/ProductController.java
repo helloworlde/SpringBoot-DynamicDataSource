267e9c18dcb30767026a3aa735938ff54f39fd5d
何惠民 <huimin.he@yi23.net>
2017-12-30 23:15:45 +0800

    Correct product controller

--- src/main/java/cn/com/hellowood/dynamicdatasource/controller/ProductController.java
+++ src/main/java/cn/com/hellowood/dynamicdatasource/controller/ProductController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
-public class ProduceController {
+public class ProductController {
 
     @Autowired
     private ProductService productService;
